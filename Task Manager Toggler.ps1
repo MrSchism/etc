be3c55e5d150da2eee04eda7f98f5db227d01136
Joshua Embrey <mrschism@sdf.org>
2014-02-05 02:57:54 -0500

    Finished it
    Made it work

--- Task Manager Toggler.ps1
+++ Task Manager Toggler.ps1
@@ -1,6 +1,12 @@
-# Toggle feature
-# set-itemproperty -path $key -name DisableTaskMgr -value 1
- 
+######################################################################################
+# Task Manager Toggler written by: 									
+#   Joshua "MrSchism" Embrey [mrschism@sdf.org]					
+#   Joseph "Arcarna" Preston [jpreston86@gmail.com]					
+# Intial commit: February 5, 2014 							
+# Current version: February 5, 2014							
+######################################################################################
+
+# Variables
 $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System\'
 $tog = '0'
 $ans = ""
@@ -12,13 +18,14 @@ $title = {########################
 
 } 
 
-
+# Start clean
 cls
 
+# Throw the title
 $title
 
- 
-if (!(Get-ItemProperty $key -Name DisableTaskMgr -ea 0)){
+# Check for key existence and offer to make it if absent.
+if(!(Get-ItemProperty $key -Name DisableTaskMgr -ea 0)){
 'Task Manager is not controlled by registry'
 'would you like to create the registry value?'
 '1. yes'
@@ -48,23 +55,56 @@ switch ($ans) {
   }
 }
 
-if (((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 0) {
-  'Task Manager is currently [ENABLED]'
-  $tog = 0}
-elseif (((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 1) {
-  'Task Manager is currently [DISABLED]'
-  $tog = 1}
+# Describes if the task manager is enabled and sets the toggle ($tog) variable
+if(((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 0) {
+'Task Manager is currently [ENABLED]'
+$tog = 0}
+elseif(((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 1) {
+'Task Manager is currently [DISABLED]'
+$tog = 1}
 
+# Offers to enable or disable based on the state of $tog
 if ($tog -eq 0) {
-    'Would you like to DISABLE Task Manager?'
+    'Would you like to disable Task Manager?'
     ''
-    "1. yes"
+    '1. yes'
     '2. no'
-    
+    $zen = read-host
+        switch ($zen) {
+            "yes" {set-itemproperty -path $key -name DisableTaskMgr -value 1
+                    'Task Manager has been DISABLED.'
+                    }
+            1 {set-itemproperty -path $key -name DisableTaskMgr -value 1
+                    'Task Manager has been DISABLED.'
+                    }
+            "no" {"Feel free to change your mind any time..."
+                    exit}
+            2 {"Feel free to change your mind any time..."
+                    exit}
+            default {"Feel free to change your mind any time..."
+                    exit}
+
+  }
 }
 if ($tog -eq 1) {
     'Would you like to ENABLE Task Manager?'
     ''
     '1. yes'
     '2. no'
+    $zen = read-host
+        switch ($zen) {
+            "yes" {set-itemproperty -path $key -name DisableTaskMgr -value 0
+                    'Task Manager has been ENABLED.'
+                    }
+            1 {set-itemproperty -path $key -name DisableTaskMgr -value 0
+                    'Task Manager has been ENABLED.'
+                    }
+            "no" {"Feel free to change your mind any time..."
+                    exit}
+            2 {"Feel free to change your mind any time..."
+                    exit}
+            default {"Feel free to change your mind any time..."
+                    exit}
+
+}
 }
