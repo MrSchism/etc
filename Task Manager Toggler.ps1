09b364af6542cea84e539bd28361efd5a47a2e4c
Joshua Embrey <mrschism@sdf.org>
2014-02-12 02:06:07 -0500

    Improved functionality and layout
    1. Added an check to squash an error prompting people to change a non-existent registry value
    2. Included a whitelist array ($valid) that checks entered choices against known good
    3. Improved line spacing and indentation

--- Task Manager Toggler.ps1
+++ Task Manager Toggler.ps1
@@ -11,6 +11,10 @@ $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System\'
 $tog = 0
 $ans = ""
 $zen = ""
+$avail = 1
+
+# Whitelist array for input
+$valid = "yes","no","y","n",1,2
 
 $title = {########################
 # Task Manager Toggler #
@@ -26,98 +30,103 @@ $title
 
 # Check for key existence and offer to make it if absent.
 if(!(Get-ItemProperty $key -Name DisableTaskMgr -ea 0)){
+    $avail = 0
     'Task Manager is not controlled by registry.'
     'Would you like to create the registry value?'
     ''
     '1. yes'
     '2. no'
-    $ans = read-host
-
+    ''
+    while ($valid -notcontains $ans) {
+        $ans = read-host
+    }
+    
     switch ($ans) {
         "yes" {(md $key) > $NULL
             cls
+            $avail = 1
             New-ItemProperty "$key" -Name "DisableTaskMgr" -Value 0 -PropertyType "DWord" > $null
             $title
             "Task Manager is now able to be toggled."
             ''
         }
         
-        1 {md $key > $null
+        1 {(md $key) > $null
             cls
+            $avail = 1
             New-ItemProperty "$key" -Name "DisableTaskMgr" -Value 0 -PropertyType "DWord" >$null
             $title
             "Task Manager is now able to be toggled."
             ''
         }
-        "no" {"Feel free to change your mind any time..."
-            exit}
-        2 {"Feel free to change your mind any time..."
-            exit}
-        default {"Feel free to change your mind any time..."
-            exit}
+        "no" {'';"Feel free to change your mind any time..."}
+        2 {'';"Feel free to change your mind any time..."}
+        default {'';"Feel free to change your mind any time..."}
 
     }
 }
 
-# Describes if the task manager is enabled and sets the toggle ($tog) variable
-if(((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 0) {
-    'Task Manager is currently [ENABLED]'
-    $tog = 0}
-elseif(((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 1) {
-    'Task Manager is currently [DISABLED]'
-    $tog = 1}
+if ($avail -eq 1) {
+    # Describes if the task manager is enabled and sets the toggle ($tog) variable
+    if(((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 0) {
+        'Task Manager is currently [ENABLED]'
+        $tog = 0}
+    elseif(((Get-ItemProperty -Path $key -Name DisableTaskMgr).DisableTaskMgr) -eq 1) {
+        'Task Manager is currently [DISABLED]'
+        $tog = 1}
 
-# Offers to enable or disable based on the state of $tog
-if ($tog -eq 0) {
-    'Would you like to DISABLE Task Manager?'
-    ''
-    '1. yes'
-    '2. no'
-    $zen = read-host
-        switch ($zen) {
-            "yes" {set-itemproperty -path $key -name DisableTaskMgr -value 1
+    # Offers to enable or disable based on the state of $tog
+    if ($tog -eq 0) {
+        'Would you like to DISABLE Task Manager?'
+        ''
+        '1. yes'
+        '2. no'
+        ''
+        while ($valid -notcontains $zen) {
+            $zen = read-host
+        }
+            switch ($zen) {
+                "yes" {set-itemproperty -path $key -name DisableTaskMgr -value 1
                     ''
                     'Task Manager has been DISABLED.'
                     }
-            1 {set-itemproperty -path $key -name DisableTaskMgr -value 1
+                1 {set-itemproperty -path $key -name DisableTaskMgr -value 1
                     ''
                     'Task Manager has been DISABLED.'
                     }
-            "no" {''
-                    "Feel free to change your mind any time..."
-                    exit}
-            2 {''
-                    "Feel free to change your mind any time..."
-                    exit}
-            default {''
-                    "Feel free to change your mind any time..."
-                    exit}
+                "no" {''
+                    "Feel free to change your mind any time..."}
+                2 {''
+                    "Feel free to change your mind any time..."}
+                default {''
+                    "Feel free to change your mind any time..."}
+        }
     }
-}
-if ($tog -eq 1) {
-    'Would you like to ENABLE Task Manager?'
-    ''
-    '1. yes'
-    '2. no'
-    $zen = read-host
-        switch ($zen) {
-            "yes" {set-itemproperty -path $key -name DisableTaskMgr -value 0
+    if ($tog -eq 1) {
+        'Would you like to ENABLE Task Manager?'
+        ''
+        '1. yes'
+        '2. no'
+        $zen = read-host
+            switch ($zen) {
+                "yes" {set-itemproperty -path $key -name DisableTaskMgr -value 0
                     ''
                     'Task Manager has been ENABLED.'
                     }
-            1 {set-itemproperty -path $key -name DisableTaskMgr -value 0
+                1 {set-itemproperty -path $key -name DisableTaskMgr -value 0
                     ''
                     'Task Manager has been ENABLED.'
                     }
-            "no" {''
+                "no" {''
                     "Feel free to change your mind any time..."
-                    exit}
-            2 {''
+                    }
+                2 {''
                     "Feel free to change your mind any time..."
-                    exit}
-            default {''
+                    }
+                default {''
                     "Feel free to change your mind any time..."
-                    exit}
+                    }
 
+        }
     }
 }
